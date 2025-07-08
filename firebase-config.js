1908293c073d0723340cf846e13f97f1ea68c2e5
xohbeats <dandrespears8@gmail.com>
2025-07-08 03:11:21 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Replace with your Firebase keys
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app-id",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "your-id",
+  appId: "your-app-id"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
